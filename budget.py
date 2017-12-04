2f0bd5655fa23d5c684dcf2089624fedceb1a236
Luke Gerhart <luke_gerhart@hotmail.com>
2017-12-04 14:20:14 -0500

    switch to comp

--- budget.py
+++ budget.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+categories = []
+
+class Category(Resource):
+	def get(self):
+		return jsonify(categories)
+		
+	def put(self, cat_name):
+		pass
+		
+	def delete(self, cat_name):
+		pass
+		
+api.add_resource(Category, '/cats')
